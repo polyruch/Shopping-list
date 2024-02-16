4c8aebe48dd3daf74f55bca62a27a75ababd9d7d
Mac <khpoly0@gmail.com>
2024-02-16 16:33:18 +0100

    added functionality to the app"

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const input = document.getElementById("item-input");
 const itemForm = document.getElementById("item-form");
 const list = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+const filterInput = document.getElementById("filter");
+
+function checkEmpty() {
+  if (list.childElementCount == 0) {
+    filterInput.style.display = "none";
+    clearBtn.style.display = "none";
+  } else {
+    filterInput.style.display = "block";
+    clearBtn.style.display = "block";
+  }
+}
 
 function handleSubmit(e) {
   e.preventDefault();
@@ -8,12 +20,14 @@ function handleSubmit(e) {
   const button = createButton("remove-item btn-link text-red");
   const icon = createIcon("fa-solid fa-xmark");
 
-  button.appendChild(icon);
-  li.innerText = input.value;
-  li.appendChild(button);
-  list.appendChild(li);
-
-  console.log(li);
+  if (input.value) {
+    button.appendChild(icon);
+    li.innerText = input.value;
+    li.appendChild(button);
+    list.appendChild(li);
+    input.value = "";
+    checkEmpty();
+  }
 }
 
 function createButton(classes) {
@@ -28,4 +42,39 @@ function createIcon(classes) {
   return icon;
 }
 
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure ?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+  checkEmpty();
+}
+
+function handleFilter(e) {
+  const textToFilter = e.target.value.toLowerCase();
+  const liItems = document.querySelectorAll("li");
+  liItems.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+    if (itemName.indexOf(textToFilter) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+function clearAll() {
+  if (confirm("Are you sure you want to delete all tasks ?")) {
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
+    checkEmpty();
+  }
+}
+
 itemForm.addEventListener("submit", handleSubmit);
+list.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearAll);
+filterInput.addEventListener("input", handleFilter);
+checkEmpty();
